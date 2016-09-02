b5ebda61069f7b10c207f8c61d407b25e6dd62f6
Benjamin Abt <mail@benjamin-abt.com>
2016-09-02 14:11:07 +0200

    Add project files.

---
+++ src/AspNetCore-Mvc-TagHelper-CoreFx/Helpers/Helpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCore_Mvc_TagHelper_CoreFx.Helpers
+{
+    [HtmlTargetElement("random-string", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class RandomStringTagHelper : TagHelper
+    {
+        // attribute
+        [HtmlAttributeName(nameof(Length))]
+        public int Length { get; set; }
+
+        [HtmlAttributeName(nameof(Chars))]
+        public String Chars { get; set; }
+
+        // fields
+        private const string DefaultChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private static readonly Random Random = new Random(Guid.NewGuid().GetHashCode());
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var availableChars = !String.IsNullOrEmpty(Chars) ? Chars : DefaultChars;
+
+            IEnumerable<char> randomChars = Enumerable.Repeat(availableChars, Length)
+                                                .Select(s => s[Random.Next(s.Length)]);
+
+            string randomString = String.Concat(randomChars);
+
+            output.Content.SetContent(randomString);
+        }
+
+
+    }
+}
+
