5b9e0196ff12fa84cd0e86c0704ebbd53253ef71
Benjamin Abt <mail@benjamin-abt.com>
2016-09-07 11:39:34 +0200

    Fixed demo settings

---
+++ src/AspNetCore-Mvc-TagHelper-CoreFx/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Mvc_TagHelper_CoreFx.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
