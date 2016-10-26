5e15f2dab0510be3930745f19d334e4ba27aa200
Benjamin Abt <mail@benjamin-abt.com>
2016-10-26 17:06:15 +0200

    Added Insight sample

---
+++ src/AspNetCore-Mvc-Insights-CoreFx/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Mvc_Insights_CoreFx.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            TelemetryClient client = new TelemetryClient();
+
+            client.TrackTrace("test");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
